fe2622f27a89e73b36c313c89b151dd51495d569
vongkeo <vongkeokeosavanh@gmail.com>
2020-06-09 15:24:33 +0700

    slider is done, but the slide of the navigation have to check it again

--- components/banner/slide1.vue
+++ components/banner/slide1.vue
@@ -2,7 +2,7 @@
   <v-carousel-item src="/banner/banner-1.png">
     <v-sheet height="100%" tile>
       <v-row class="fill-height" align="center" justify="center">
-        <v-card flat>
+        <v-card flat max-width="1000">
           <v-card-title class="display-2 font-weight-bold">Get</v-card-title>
           <v-card-title class="flex display-4 font-weight-black color-pink"
             >$2000</v-card-title
@@ -12,18 +12,18 @@
           >
 
           <v-card-title class="headline font-weight-light">
-            Use bonus code:
-            <v-chip color="white" outlined class="ma-2 ec-welcome display-1">
+            Use bonus code :
+            <v-chip outlined class="ma-2 ec-welcome white--text display-1">
               ecwelcome
             </v-chip></v-card-title
           >
           <v-card-actions class=" d-block">
-            <v-row>
+            <v-row class="container-list-icon mb-4">
               <v-col cols="4" v-for="(item, index) in titleDetial" :key="index">
-                <v-list-item two-line>
-                  <v-list-item-icon>
-                    <v-icon>fas fa-chevron-double-right</v-icon>
-                  </v-list-item-icon>
+                <v-list-item two-line class="cover-icon px-0">
+                  <v-list-item-avatar width="60">
+                    <v-img :class="item.class" :src="item.src"></v-img>
+                  </v-list-item-avatar>
                   <v-list-item-content>
                     <v-list-item-title class="  text-uppercase">{{
                       item.title
