4f54df488846715ca1f2bdb39927feea0e95ebd6
vongkeo <vongkeokeosavanh@gmail.com>
2020-06-11 13:36:13 +0700

    profile fix design and put the background is done

---
+++ components/profile/subheader.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-subheader class=" high-0 text-capitalize">{{ title }}</v-subheader>
+</template>
+
+<script>
+export default {
+  props: ["title"]
+};
+</script>
+
+<style></style>
