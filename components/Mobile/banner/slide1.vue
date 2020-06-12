09939f5aa802f0dfc69318ff7fc74b42d5f52e75
Ritesh <riteshtnk11@gmail.com>
2020-06-12 13:08:49 +0700

    Mobile responsive design for slide1

--- components/Mobile/banner/slide1.vue
+++ components/Mobile/banner/slide1.vue
@@ -2,41 +2,39 @@
   <v-carousel-item src="/banner/banner-1.png">
     <v-sheet height="100%" tile>
       <v-row class="fill-height" align="center" justify="center">
-        <v-card flat max-width="1000" class=" text-center">
-          <v-card-title class="display-2 font-weight-bold">Get</v-card-title>
-          <v-card-title class="flex display-4 font-weight-black color-pink"
-            >$2000</v-card-title
-          >
-          <v-card-title class="display-3 .font-weight-medium"
+        <v-card flat max-width="1000" class="text-center mb-5" style="margin-top:60px;">
+          <v-card class="display-2 font-weight-bold text-center">Get</v-card>
+          <v-card-text style="color: #e51267;" class="flex display-2 font-weight-bold text-center"
+            >$2000
+          </v-card-text>
+          <v-card-title class="display-1 font-weight-bold"
             >Get a WELCOME 200% Bonus up to $2000</v-card-title
           >
-
-          <v-card-title class="headline font-weight-light">
+          <v-card class="headline font-weight-light text-center text-center">
             Use bonus code :
-            <v-chip outlined class="ma-2 ec-welcome white--text display-1">
-              ecwelcome
-            </v-chip></v-card-title
-          >
-          <v-card-actions class=" d-block">
-            <v-row class="container-list-icon mb-4">
-              <v-col cols="4" v-for="(item, index) in titleDetial" :key="index">
-                <v-list-item two-line class="cover-icon px-0">
+          </v-card>
+          <v-chip outlined class="ma-2 ec-welcome white--text display-1 t">
+            ecwelcome
+          </v-chip>
+          <v-card-actions class="d-block">
+            <v-card class="container-list-icon">
+              <v-card v-for="(item, index) in titleDetail" :key="index">
+                <v-list-item two-line class="cover-icon justify-center">
                   <v-list-item-avatar width="60">
-                    <v-img :class="item.class" :src="item.src"></v-img>
+                    <v-img :class="item.class" style="height: 45px;" :src="item.src"></v-img>
                   </v-list-item-avatar>
+                </v-list-item>
+                <v-list-item>
                   <v-list-item-content>
-                    <v-list-item-title class="  text-uppercase">{{
-                      item.title
-                    }}</v-list-item-title>
-                    <v-list-item-subtitle class="text-uppercase">{{
-                      item.desc
-                    }}</v-list-item-subtitle>
+                    <v-list-item-title class="text-uppercase">
+                      {{ item.title }} {{ item.desc }}
+                    </v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
-              </v-col>
-            </v-row>
-            <v-btn x-large rounded color="pink" class="px-8"
-              >Join now
+              </v-card>
+            </v-card>
+            <v-btn x-large rounded color="pink" class="px-8 mb-5"
+              >Bet now
               <span class=" ml-3">
                 <v-icon size="15"> fas fa-chevron-double-right</v-icon
                 ><v-icon size="15" class=" opcity-1">
@@ -56,7 +54,7 @@ import json from "~/json/items";
 export default {
   data() {
     return {
-      titleDetial: json.slideItems
+      titleDetail: json.slideItems
     };
   }
 };
